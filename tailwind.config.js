bce6004a86934c7c9b2f89c0b1c3953d45c18779
Larissa Faria <lari.f4ria@gmail.com>
2024-05-30 15:26:53 -0300

    feat: navbar and part of left side made

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}"
+  ],
+  theme: {
+    fontFamily:{
+      'inter':['Inter']
+    },
+    letterSpacing:{
+      eight:'.08em',
+      widest: '0.1em',
+      double:'0.2em'
+    },
+    extend: {
+      boxShadow:{
+        'btn':'0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1), 0 8px 10px -1px rgba(0, 0, 0, 0.1), 0 -2px 4px rgba(0, 0, 0, 0.1)',
+      }
+    },
+  },
+  plugins: [],
+}
+
